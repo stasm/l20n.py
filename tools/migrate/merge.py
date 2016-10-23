bb1907bb551b8be93f2d69a5e6469f00ae0626a2
Staś Małolepszy <stas@mozilla.com>
2016-10-23 22:24:28 +0200

    MergeContext

--- tools/migrate/merge.py
+++ tools/migrate/merge.py
@@ -1,6 +1,15 @@
 # coding=utf8
 
+import os
+import codecs
+import logging
+
 import l20n.format.ast as FTL
+from l20n.format.parser import FTLParser
+from l20n.format.serializer import FTLSerializer
+from compare_locales.parser import getParser
+
+from operations import VARIANTS
 
 
 def get_entity(body, ident):
@@ -35,3 +44,123 @@ def merge(reference, localization, legacy, transforms):
     ]
 
     return FTL.Resource(body, reference.comment)
+
+
+class MergeContext(object):
+    """Stateful context for merging translation resources.
+
+    `MergeContext` must be configured with the target language and the
+    directory locations of the input data.
+
+    The transformation takes four types of input data:
+
+        - The en-US FTL reference files which will be used as templates for
+          message order, comments and sections.
+
+        - The current FTL files for the given language.
+
+        - The legacy (DTD, properties) translation files for the given
+          language.  The translations from these files will be transformed
+          into FTL and merged into the existing FTL files for this language.
+
+        - A list of `FTL.Entity` objects whose some nodes are special operation
+          nodes: COPY, REPLACE, PLURALS, CONCAT, INTERPOLATE.
+    """
+
+    def __init__(self, lang, reference_dir, localization_dir):
+        self.ftl_parser = FTLParser()
+        self.ftl_serializer = FTLSerializer()
+
+        # An iterable of plural category names relevant to the context's
+        # language.  E.g. ('one', 'other') for English.
+        self.plural_categories = ('one', 'other')
+
+        # Paths to directories with input data, relative to CWD.
+        self.reference_dir = reference_dir
+        self.localization_dir = localization_dir
+
+        # Parsed input resources stored by resource path.
+        self.reference_resources = {}
+        self.current_resources = {}
+        self.legacy_resources = {}
+
+        # An iterable of `FTL.Entity` object whose some nodes can be the
+        # transform operations: COPY, REPLACE, PLURALS, CONCAT, INTERPOLATE.
+        self.transforms = {}
+
+    def read_ftl_resource(self, path):
+        f = codecs.open(path, 'r', 'utf8')
+        try:
+            contents = f.read()
+        except UnicodeDecodeError, err:
+            logger = logging.getLogger('migrate')
+            logger.error(u'Error reading file {}: {}'.format(path, str(err)))
+        f.close()
+
+        ast, errors = self.ftl_parser.parse(contents)
+
+        if len(errors):
+            logger = logging.getLogger('migrate')
+            for err in errors:
+                logger.error(u'Syntax error in {}: {}'.format(path, str(err)))
+
+        return ast
+
+    def add_reference(self, path):
+        """Add an FTL AST to this context's reference resources."""
+        fullpath = os.path.join(self.reference_dir, path)
+        self.reference_resources[path] = self.read_ftl_resource(fullpath)
+
+    def add_current(self, path):
+        """Add an FTL AST to this context's current localization resources."""
+        fullpath = os.path.join(self.localization_dir, path)
+        self.current_resources[path] = self.read_ftl_resource(fullpath)
+
+    def add_legacy(self, path):
+        """Add a dict to this context's legacy localization resources."""
+        fullpath = os.path.join(self.localization_dir, path)
+        parser = getParser(fullpath)
+        parser.readFile(fullpath)
+        # Transform the parsed result which is an iterator into a dict.
+        collection = {ent.get_key(): ent for ent in parser}
+        self.legacy_resources[path] = collection
+
+    def add_transforms(self, path, transforms):
+        """Define transforms for `path`."""
+        self.transforms[path] = transforms
+
+    def create_source(self):
+        """Create a SOURCE partial for use with other operations."""
+        def source(path, key):
+            entity = self.legacy_resources[path].get(key, None)
+            if entity is not None:
+                return entity.get_val()
+        return source
+
+    def create_plurals(self):
+        """Create a PLURALS partial for use with other operations."""
+        def plurals(source, selector, foreach):
+            return VARIANTS(source, selector, self.plural_categories, foreach)
+        return plurals
+
+    def merge(self, changeset):
+        """Transform and merge context's input data.
+
+        The input data must be configured earlier using the `add_*` methods.
+        The `changeset` argument is a dict of (file path, list of keys)
+        describing which legacy traslations should be transformed.
+        """
+
+        # XXX Filter legacy_resources according to changeset.
+        changed = self.legacy_resources
+
+        result = {}
+
+        for path, reference in self.reference_resources.iteritems():
+            current = self.current_resources.get(path, FTL.Resource())
+            transforms = self.transforms.get(path, [])
+
+            merged = merge(reference, current, changed, transforms)
+            result[path] = self.ftl_serializer.serialize(merged.toJSON())
+
+        return result
---
+++ tools/migrate/test_context.py
@@ -0,0 +1,61 @@
+# coding=utf8
+
+import os
+import unittest
+
+from util import ftl
+
+from merge import MergeContext
+
+import l20n.format.ast as FTL
+from operations import COPY
+
+
+def path(*args):
+    return os.path.join(os.path.dirname(os.path.realpath(__file__)), *args)
+
+
+class TestMergeContext(unittest.TestCase):
+    def setUp(self):
+        self.ctx = MergeContext(
+            lang='pl',
+            reference_dir='fixtures/en-US',
+            localization_dir='fixtures/pl'
+        )
+
+        self.ctx.add_reference('aboutDownloads.ftl')
+        self.ctx.add_legacy('aboutDownloads.dtd')
+        self.ctx.add_legacy('aboutDownloads.properties')
+
+    def test_merge_context(self):
+        SOURCE = self.ctx.create_source()
+
+        self.ctx.add_transforms(
+            'aboutDownloads.ftl',
+            [
+                FTL.Entity(
+                    FTL.Identifier('title'),
+                    value=COPY(
+                        SOURCE(
+                            'aboutDownloads.dtd',
+                            'aboutDownloads.title'
+                        )
+                    )
+                ),
+            ]
+        )
+
+        expected = {
+            'aboutDownloads.ftl': ftl('''
+        # This Source Code Form is subject to the terms of the Mozilla Public
+        # License, v. 2.0. If a copy of the MPL was not distributed with this
+        # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+        title = Pobrane pliki
+            ''')
+        }
+
+        self.assertDictEqual(
+            self.ctx.merge(None),
+            expected
+        )
