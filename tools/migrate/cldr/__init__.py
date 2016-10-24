541cb50f6dcbd7ef51a7c37f0cbe1a69467a05ef
Staś Małolepszy <stas@mozilla.com>
2016-10-24 17:43:48 +0200

    Use canonical order for plural categories

--- tools/migrate/cldr/__init__.py
+++ tools/migrate/cldr/__init__.py
@@ -14,13 +14,20 @@ def path(*args):
 cldr_plurals = json.load(codecs.open(path('plurals.json'), 'r', 'utf8'))
 rules = cldr_plurals['supplemental']['plurals-type-cardinal']
 
+canonical_order = ('zero', 'one', 'two', 'few', 'many', 'other')
+
+
+def in_canonical_order(a, b):
+    return canonical_order.index(a) - canonical_order.index(b)
+
+
 categories = {}
 
 for lang, rules in rules.items():
-    categories[lang] = map(
+    categories[lang] = sorted(map(
         lambda key: key.replace('pluralRule-count-', ''),
         rules.keys()
-    )
+    ), in_canonical_order)
 
 
 def get_plural_categories(lang):
